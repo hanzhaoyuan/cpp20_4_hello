a98d93bb8e6de056bc516efdf9f74a8ed899d3af
boris_han <borishan@kcexfintech.com>
2026-05-11 01:09:34 +0800

    chore:访问修饰符左对齐

--- app/main.cpp
+++ app/main.cpp
@@ -1,25 +1,29 @@
-#include <array>
 #include <format>
 #include <iostream>
-#include <stdexcept>
+#include <string>
+
+class Player {
+public: // 外部可以访问
+    std::string name;
+    int health;
+
+    // 成员函数：在类内部定义行为
+    void takeDamage(int amount) {
+        health -= amount;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+};
 
 int main() {
-    std::array<int, 5> prices = {100, 101, 102, 103, 104};
+    Player p;
+    p.name = "llq";
+    p.health = 20;
+    std::cout << std::format("name: {}, health: {}", p.name, p.health) << '\n';
 
-    // 1. arr[i] 的典型用法：追求极致速度
-    // 在 HFT 中，如果你已经通过逻辑保证 i 不会越界（例如循环边界），
-    // 编译器会将此操作直接优化为单条机器指令。
-    int fast_val = prices[2];
-    std::cout << std::format("Fast access: {}\n", fast_val);
-
-    // 2. arr.at(i) 的典型用法：宁愿报错也不要崩盘
-    // 这多出的逻辑判断会破坏 CPU 的分支预测，增加纳秒级的延迟。
-    try {
-        int safe_val = prices.at(10); // 故意越界
-        std::cout << std::format("Safe access: {}\n", safe_val);
-    } catch (const std::out_of_range& e) {
-        std::cerr << std::format("Caught: {}\n", e.what());
-    }
+    p.takeDamage(30);
+    std::cout << std::format("name: {}, health: {}", p.name, p.health) << '\n';
 
     return 0;
 }
